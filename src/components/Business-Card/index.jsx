fa10040d389511c3873743c576409ab92825dc26
retr0lbb <retr0lbb@gmail.com>
2023-04-26 13:49:20 +0000

    working on cv carda

--- src/components/Business-Card/index.jsx
+++ src/components/Business-Card/index.jsx
@@ -3,13 +3,16 @@ import style from "./Business-card.module.css"
 import linkedin from'../../assets/linkedin.svg'
 import gitHub from '../../assets/github.svg'
 import instagram from'../../assets/instagram.svg'
+import henrique from'../../assets/henrique.jpg'
 const Bcard = () => {
 
     return(
         <div className={style.wrapper}>
-            <div className={style.foto}></div>
-            <div>
-                <div>
+            <div className={style.foto}>
+                <img src={henrique} alt="none"/>
+            </div>
+            <div className={style.content}>
+                <div className={style.links}>
                     <img src={linkedin} alt="none" />
                     <img src={gitHub} alt="Github" />
                     <img src={instagram} alt="Instagram" />    
